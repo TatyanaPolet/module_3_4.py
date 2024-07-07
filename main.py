89b03e7ef2191d454a25abe2bd3dda89b58c17c8
TatyanaPolet <tatyana_polet@mail.ru>
2024-07-07 11:55:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+def single_root_words(root_word, *other_words):
+    same_word = []
+    for i in other_words:
+
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
+            same_word.append(i)
+
+    return same_word
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
